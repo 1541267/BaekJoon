9d45a9c2bbe9fa0a5eb1ade369588db9a339f9fa
1541267 <1541267@naver.com>
2025-03-26 12:18:46 +0900

    (실버 3) 19637번 IF문 좀 대신 써줘

--- src/main/java/baekJoon/tier/sliver/three/WriteIf.java
+++ src/main/java/baekJoon/tier/sliver/three/WriteIf.java
@@ -71,10 +71,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 440ms
 public class WriteIf {
-
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 32768);
 
 		int n = readNumber(br);
 		int m = readNumber(br);
